5bb49681f1b479f5cc3b8d15c9a509f4ee9a1914
AvishMD <avishmd2007@gmail.com>
2026-03-05 10:20:40 +0530

    Create p9.c

---
+++ p9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n,temp;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        temp=arr[i];
+        int s=1;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]>temp){
+            s=0;
+            break;
+            }
+        }
+        if(s==1){
+             printf("%d ",arr[i]);
+    }
+    }
+    return 0;
+}

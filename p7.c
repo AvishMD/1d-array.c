43bd28a501bab154c8902a840d18cf6bbe979ef5
AvishMD <avishmd2007@gmail.com>
2026-03-05 09:47:59 +0530

    Create p7.c

---
+++ p7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n,count=0,counts=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0)
+        count++;
+        else 
+        counts++;
+    }
+    printf("Even: %d\n",count);
+    printf("Odd: %d",counts);
+    return 0;
+}

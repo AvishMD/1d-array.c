ed55d04e927eed32cdc7a44a118514d5a44ed9fe
AvishMD <avishmd2007@gmail.com>
2026-03-05 13:47:36 +0530

    Create p16.c

---
+++ p16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n,max;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    max=arr[1]-arr[0];
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]-arr[i]>max){
+                max=arr[j]-arr[i];
+            }
+        }
+    }
+    printf("%d",max);
+
+    return 0;
+}
